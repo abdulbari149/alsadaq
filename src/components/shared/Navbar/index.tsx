5d45767819bdca758ce0234eade70a8b9aea0a88
saadfrhan <saadfarhan347@gmail.com>
2024-08-17 23:57:59 +0500

    ui fix

--- src/components/shared/Navbar/index.tsx
+++ src/components/shared/Navbar/index.tsx
@@ -6,7 +6,7 @@ import { routes } from "./routes";
 
 const Navbar = () => {
 	return (
-		<header className="flex flex-row max-w-[86rem] px-6 w-full mx-auto justify-between bg-white border-solid border-[#E9EDEE]">
+		<header className="flex flex-row max-w-[86rem] px-16 max-sm:px-6 w-full mx-auto justify-between bg-white border-solid border-[#E9EDEE]">
 			{/* <h2 className="text-[24px] font-[600] text-[#333839]">Al-Sadaq</h2> */}
 			<Image src="/logo.svg" className="my-5" alt="al sadaq" width={72} height={42} />
 			<nav className="flex flex-row gap-8 max-md:hidden">
