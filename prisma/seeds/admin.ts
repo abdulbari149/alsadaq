c262fd5e4e78d889ae7618f139619ae069ea3df5
abdulbari149 <abdulbari131103@gmail.com>
2024-08-24 17:27:59 +0500

    feat: added new admin user

--- prisma/seeds/admin.ts
+++ prisma/seeds/admin.ts
@@ -1,7 +1,7 @@
-import { type PrismaClient } from '@prisma/client';
-import bcrypt from 'bcrypt';
-import { type Seeder } from '../../src/types/seeder.type';
-import admins from '../../data/admins.json'
+import { type PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+import { type Seeder } from "../../src/types/seeder.type";
+import admins from "../../data/admins.json" with { type: "json" };
 
 class SuperAdminSeeder implements Seeder {
   isExecutable: boolean = true;
@@ -11,21 +11,24 @@ class SuperAdminSeeder implements Seeder {
       await Promise.all(
         admins.map(async (admin) => {
           const password = await bcrypt.hash(admin.password, 10);
-          return await prisma.user.create({
+          const user = await prisma.user.findFirst({
+            where: { email: admin.email },
+          });
+
+          if (user) {
+            await prisma.user.update({
+              where: { id: user.id },
+              data: { role: "admin", password, username: admin.username },
+            });
+            return;
+          }
+
+          await prisma.user.create({
             data: {
               email: admin.email,
               username: admin.username,
               password,
               role: "admin",
-            },
-            select: {
-              id: true,
-              username: true,
-              email: true,
-              password: true,
-              createdAt: true,
-              role: true,
-              updatedAt: true,
             },
           });
         })
