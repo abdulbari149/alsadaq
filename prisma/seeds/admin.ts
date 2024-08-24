59fe5c55aa54c4bd13ecc4c81f71a3e91f5b8779
abdulbari149 <abdulbari131103@gmail.com>
2024-08-25 00:04:32 +0500

    feat: fetching admins json from supabase

--- prisma/seeds/admin.ts
+++ prisma/seeds/admin.ts
@@ -2,14 +2,32 @@ import { type PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
 import { type Seeder } from "../../src/types/seeder.type";
 import admins from "../../data/admins.json" with { type: "json" };
+import { supabase } from "@/lib/supabase";
+
 
 class SuperAdminSeeder implements Seeder {
   isExecutable: boolean = true;
 
+  private async fetchAdmins() {
+
+    const { data } = await supabase.storage.from('data').download('admins.json')
+
+    const bufferData = await data?.arrayBuffer()
+    if (!bufferData) {
+      throw new Error('Invalid buffer data');
+    };
+    const values = Buffer.from(bufferData).toString('utf-8')
+    const admins = JSON.parse(values);
+
+    return admins;
+  }
+
   async seed(prisma: PrismaClient): Promise<void> {
+
     try {
+      const admins = await this.fetchAdmins();
       await Promise.all(
-        admins.map(async (admin) => {
+        admins.map(async (admin: { email: string, username: string, password: string }) => {
           const password = await bcrypt.hash(admin.password, 10);
           const user = await prisma.user.findFirst({
             where: { email: admin.email },
