415ce30ffe5e43331ae68c843a713dc229642bf2
abdulbari149 <abdulbari131103@gmail.com>
2024-11-16 20:16:27 +0500

    feat: google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,10 @@ import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Script from "next/script";
 
 const font = Plus_Jakarta_Sans({ subsets: ["latin"] });
+const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID; // Replace with your actual GA tracking ID
 
 export default function RootLayout({
 	children,
@@ -12,6 +14,24 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
+			<head>
+				<Script
+					async
+					src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+				/>
+				<Script
+					id="google-analytics"
+					strategy="afterInteractive"
+					dangerouslySetInnerHTML={{
+						__html: `
+							window.dataLayer = window.dataLayer || [];
+							function gtag(){dataLayer.push(arguments);}
+							gtag('js', new Date());
+							gtag('config', '${GA_TRACKING_ID}');
+						`,
+					}}
+				/>
+			</head>
 			<body className={font.className}>
 				{children}
 				<ToastContainer />
